bf0b075708f23e68a901c6d4afb5d32405c07bc0
ajayajju2412 <ajayajju2412@github.com>
2020-09-16 14:34:50 +0530

    Checkpoint2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
-import { SignInComponent } from './shared/sign-in/sign-in.component';
-import { HomeComponent } from './home/home/home.component';
-import { FooterComponent } from './core/footer/footer.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    SignInComponent,
-    HomeComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HomeModule,
+    CoreModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
